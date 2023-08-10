c2d1d4489bd64f957c2a0f49e4594be2b4034397
Pedro Henrique <p13dr0h@gmail.com>
2023-08-10 02:20:43 -0300

    Initial commit

---
+++ src/main/java/br/com/ph/messagesb/MessageSbApplication.java
@@ -0,0 +1,13 @@
+package br.com.ph.messagesb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ph/messagesb/MessageSbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ph.messagesb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
