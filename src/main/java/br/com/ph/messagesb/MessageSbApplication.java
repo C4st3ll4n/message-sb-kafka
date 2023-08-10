22e7e6fad96a9021a046855d4ac84f43a49aca16
Pedro Henrique <p13dr0h@gmail.com>
2023-08-10 03:16:03 -0300

    feat: add endpoint to receive requests

--- src/main/java/br/com/ph/messagesb/MessageSbApplication.java
+++ src/main/java/br/com/ph/messagesb/MessageSbApplication.java
@@ -2,6 +2,10 @@ package br.com.ph.messagesb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MessageSbApplication {
@@ -11,3 +15,22 @@ public class MessageSbApplication {
 	}
 
 }
+
+@RestController
+@RequestMapping("/kafka")
+class HelloController{
+	private MessageProducer service;
+
+	public HelloController(MessageProducer service) {
+		this.service = service;
+	}
+
+	@GetMapping("/{name}")
+	public String hello(@PathVariable(name = "name") String name){
+		service.sendMessage("Hello "+ name);
+		return "OK";
+	}
+}
+
+
+
